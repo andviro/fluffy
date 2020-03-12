be600f2c1d2d1c3f3ba1fa66452f5a34b3320891
Andrew Rodionoff <andviro@gmail.com>
2020-03-13 00:58:10 +0300

    added terms plot

--- plot/variable.go
+++ plot/variable.go
@@ -9,6 +9,27 @@ import (
 	"github.com/andviro/fluffy"
 )
 
+func Terms(fn string, xmin, xmax float64, terms []fluffy.Term) error {
+	graph := chart.Chart{}
+	f, err := os.Create(fn)
+	if err != nil {
+		return fmt.Errorf("creating output file: %w", err)
+	}
+	defer f.Close()
+	for _, v := range terms {
+		s := chart.ContinuousSeries{
+			Name: string(v.Name),
+		}
+		for x := xmin; x <= xmax; x += (xmax - xmin) / 100.0 {
+			s.XValues = append(s.XValues, x)
+			s.YValues = append(s.YValues, v.MembershipValue(x))
+		}
+		graph.Series = append(graph.Series, s)
+	}
+	graph.Elements = append(graph.Elements, chart.Legend(&graph))
+	return graph.Render(chart.PNG, f)
+}
+
 func MembershipFunctions(fn string, src fluffy.Variable) error {
 	graph := chart.Chart{}
 	f, err := os.Create(fn)
